4fbc31c6e2244d2a1fa4c8c194fc1167ff398ca3
Tejas Kshirsagar <tejkshirsagar@gmail.com>
2024-08-23 20:27:14 +0530

    Hello World test

---
+++ C Project 1/C Project 1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Project 1
+//
+//  Created by Tejas Kshirsagar on 23/08/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
